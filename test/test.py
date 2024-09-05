957f35d3ab304e52ff1c6c79420f914c8a459d72
0zean <42080997+0zean@users.noreply.github.com>
2024-09-05 02:22:48 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from hellingerforest import RandomForestClassifier
+from hellingerforest import RandomForestClassifier as hf
 
-data = pd.read_csv("creditcard.csv")
+data = pd.read_csv("test\creditcard.csv")
 
 X = np.array(data.iloc[260000:, 1:30])
 y = np.array(data.iloc[260000:, 30]).astype(float)
@@ -12,7 +12,7 @@ print("\nTarget:\n", y[:10])
 
 print("\n Shape of X:", X.shape)
 
-model = RandomForestClassifier(n_estimators=10, max_depth=7, min_samples_split=10)
+model = hf(n_estimators=10, max_depth=7, min_samples_split=10)
 model.fit(X, y)
 
 print("Model trained")
@@ -22,8 +22,6 @@ predictions = model.predict(X)
 print("Predictions:\n", predictions)
 
 print("\nUnique predictions:", np.unique(predictions))
-
-print("\nAccuracy:", np.mean(predictions == y))
 
 print(np.where(np.array(predictions) > 0))
 print(np.where(y > 0))
